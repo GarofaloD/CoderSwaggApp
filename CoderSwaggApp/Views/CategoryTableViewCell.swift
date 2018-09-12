e79327240f84b12fa615ce95a12091f2493c4ffc
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro.local>
2018-09-12 17:30:59 -0500

    table view and proto cell assembled

---
+++ CoderSwaggApp/Views/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  CoderSwaggApp
+//
+//  Created by Daniel Garofalo on 9/12/18.
+//  Copyright © 2018 Daniel Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
